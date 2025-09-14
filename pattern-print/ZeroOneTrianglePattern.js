cccacacfb1ed7de6eaef3b2e4fde799c788b9e1d
jayarani.ys <jayaraniys1@gmail.com>
2025-09-14 17:21:21 +0530

    zero to one triangle print

---
+++ pattern-print/ZeroOneTrianglePattern.js
@@ -0,0 +1,16 @@
+function ZeroOneTriangle(totalRows){
+
+for(let i=1 ; i<= totalRows; i++){
+    let row = "";
+    for(let j=1; j<=i; j++){
+        if((i+j)%2  == 0){
+            row+='1';
+        } else {
+            row+='0';
+        }
+    }
+    console.log(row);
+}
+
+}
+ZeroOneTriangle(5);
